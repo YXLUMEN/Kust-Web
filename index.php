68725c2e3bb6bf176b746f5e0afe0888864d78bc
YXLUMEN <lumengrace@outlook.com>
2024-09-08 22:06:36 +0800

    精简版本一

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require'./conn.php';		//引入公共文件开启session功能
-require'./src/index.html';	//调用内容模板
-
-
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require './conn.php';
-unset($_SESSION['user']);		//用户退出功能
-header('Location:index.php');	//跳转页面
-
